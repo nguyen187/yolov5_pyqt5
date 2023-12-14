c54bc15a919c74ca21c89d49f3f462e739ede6ac
nguyen187 <thanhnguyen187201@gmail.com>
2023-12-14 11:36:16 +0700

    #1 add Readme

---
+++ utils/capnums.py
@@ -0,0 +1,41 @@
+import cv2
+
+
+class Camera:
+    def __init__(self, cam_preset_num=5):
+        self.cam_preset_num = cam_preset_num
+
+    # def get_cam_num(self):
+    #     cnt = 0
+    #     devices = []
+    #     for device in range(0, self.cam_preset_num):
+    #         stream = cv2.VideoCapture(device, cv2.CAP_DSHOW)
+    #         grabbed = stream.grab()
+    #         stream.release()
+    #         if not grabbed:
+    #             continue
+    #         else:
+    #             cnt = cnt + 1
+    #             devices.append(device)
+    #     return cnt, devices
+    def get_cam_num(self):
+        cnt = 0
+        devices = []
+        for device in range(0, 10):  # Increase the range if necessary
+            print(f"Checking device {device}")
+            stream = cv2.VideoCapture(device)
+            grabbed = stream.grab()
+            stream.release()
+            if not grabbed:
+                print(f"Device {device} is not a valid camera")
+                continue
+            else:
+                cnt = cnt + 1
+                devices.append(device)
+        return cnt, devices
+
+
+if __name__ == '__main__':
+    cam = Camera()
+    cam_num, devices = cam.get_cam_num()
+    print(cam_num, devices)
